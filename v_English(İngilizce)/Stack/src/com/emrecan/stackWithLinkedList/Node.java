bddd557cd767567714bdbfd7ecd66c08e4854612
emrecanAy <codemrecan@gmail.com>
2021-11-20 22:15:58 +0300

    javadocs added!

---
+++ v_English(İngilizce)/Stack/src/com/emrecan/stackWithLinkedList/Node.java
@@ -0,0 +1,5 @@
+package com.emrecan.stackWithLinkedList;/**
+ * @author Emrecan
+ * @created 20/11/2021 - 21:32
+ */public class Node {
+}
---
+++ v_English(İngilizce)/Stack/src/com/emrecan/stackWithLinkedList/StackADT.java
@@ -0,0 +1,5 @@
+package com.emrecan.stackWithLinkedList;/**
+ * @author Emrecan
+ * @created 20/11/2021 - 21:35
+ */public class StackADT {
+}
